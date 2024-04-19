db47d502fc4a34e09370e694e76e4e3f77d0bb2a
DMGamage <gamagemadhushan@gmail.com>
2024-04-19 14:48:53 +0530

    Fix some errors

--- src/main.py
+++ src/main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
 import numpy as np
-import pandas as pd
+import pandas as pd 
 from fastapi.middleware.cors import CORSMiddleware
-from prediction_model.predict import generate_predictions
+from prediction_model.predict import generate_predictions 
 
 app = FastAPI(
-    title = "Loan Prediction app using API- CI CD jenkins",
-    description= "A simple CI CD demo",
+    title="Loan Prediction App using API - CI CD Jenkins",
+    description = "A Simple CI CD Demo",
     version='1.0'
 )
 
@@ -21,77 +21,64 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers = ["*"]
-
+    allow_headers=["*"]
 )
 
 class LoanPrediction(BaseModel):
-    Gender : str
-    Married : str
-    Dependents : str
-    Education : str
-    Self_Employed : str
-    ApplicantIncome : float
-    CoapplicantIncome : float
-    LoanAmount : float
-    Loan_Amount_Term : float
-    Credit_History : str
-    Property_Area : str
-
-     
+    Gender: str
+    Married: str
+    Dependents: str
+    Education: str
+    Self_Employed: str
+    ApplicantIncome: float
+    CoapplicantIncome: float
+    LoanAmount: float
+    Loan_Amount_Term: float
+    Credit_History: float
+    Property_Area: str
 
 
 @app.get("/")
 def index():
-    return {"message":"Welocme Loan"}
+    return {"message":"Welcome to Loan Prediction App using API - CI CD Jenkins" }
 
-@app.post("/prediction_status")
-def predict(loan_details:LoanPrediction):
-    data = loan_details.model_dumb()
+@app.post("/prediction_api")
+def predict(loan_details: LoanPrediction):
+    data = loan_details.model_dump()
     prediction = generate_predictions([data])["prediction"][0]
-
     if prediction == "Y":
         pred = "Approved"
-    else :
+    else:
         pred = "Rejected"
-
     return {"status":pred}
 
-@app.post("prediction_ui")
-def predict_gui(
-    Gender : str,
-    Married : str,
-    Dependents : str,
-    Education : str,
-    Self_Employed : str,
-    ApplicantIncome : float,
-    CoapplicantIncome : float,
-    LoanAmount : float,
-    Loan_Amount_Term : float,
-    Credit_History : str,
-    Property_Area : str):
+@app.post("/prediction_ui")
+def predict_gui(Gender: str,
+    Married: str,
+    Dependents: str,
+    Education: str,
+    Self_Employed: str,
+    ApplicantIncome: float,
+    CoapplicantIncome: float,
+    LoanAmount: float,
+    Loan_Amount_Term: float,
+    Credit_History: float,
+    Property_Area: str):
 
-    input_data=[Gender,Married,Dependents,Education,Self_Employed,ApplicantIncome,CoapplicantIncome,
-                LoanAmount,Loan_Amount_Term,Credit_History,Property_Area]
-
+    input_data = [Gender, Married,Dependents, Education, Self_Employed,ApplicantIncome,
+     CoapplicantIncome,LoanAmount, Loan_Amount_Term,Credit_History, Property_Area  ]
+    
     cols = ['Gender', 'Married', 'Dependents', 'Education',
        'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount',
        'Loan_Amount_Term', 'Credit_History', 'Property_Area']
     
     data_dict = dict(zip(cols,input_data))
-    prediction = generate_predictions([data])["prediction"][0]
-
+    prediction = generate_predictions([data_dict])["prediction"][0]
     if prediction == "Y":
         pred = "Approved"
-    else :
+    else:
         pred = "Rejected"
-
     return {"status":pred}
 
-if __name__ == "__main__":
-    uvicorn.run(app)
-
-
-
-
-
+if __name__== "__main__":
+    uvicorn.run(app, host="0.0.0.0",port=8005)
